78c5708303b24d41fd17fe10a63ad1edbfa2f2b5
Sergey Voronov <rdkl.hrd@gmail.com>
2016-09-26 08:38:58 -0400

    EDF scheduler

---
+++ real-time systems/schedulers/edf_scheduler.py
@@ -0,0 +1,19 @@
+import dynamic_scheduler
+
+
+################################################################################
+class EDFscheduler(dynamic_scheduler.DynamicScheduler):
+    def __init__(self):
+        dynamic_scheduler.DynamicScheduler.__init__(self)
+
+    # --------------------------------------------------------------------------
+    def compute_job_priority(self, job):
+        # Less priority is better.
+        return int((job.deadline + job.release) * 10 ** 8) + int(job.release)
+
+    # --------------------------------------------------------------------------
+    def compute_task_priorities(self, task_list):
+        pass
+
+    # --------------------------------------------------------------------------
+################################################################################
