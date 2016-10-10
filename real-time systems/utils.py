5fbda7de47c43340248b1596f3371af2c091761e
Sergey Voronov <rdkl.hrd@gmail.com>
2016-10-10 14:06:09 -0400

    Added breakdown utilization for task set

--- real-time systems/utils.py
+++ real-time systems/utils.py
@@ -1,3 +1,8 @@
+import copy
+
+from schedulers.edf_scheduler import EDFscheduler
+
+
 # ------------------------------------------------------------------------------
 # Colored output in console (useful for MISSED jobs).
 def print_red(string):
@@ -22,4 +27,48 @@ def lcm_args(*args):
 
 
 # ------------------------------------------------------------------------------
+def compute_utilization(task_set):
+    return sum([task.exectime / task.period for task in task_set])
+
+
+# ------------------------------------------------------------------------------
+# Assumed, that task set is schedulable.
+def compute_breakdown_utilization(task_set, context_switch_time, is_preemptive):
+    multiplier = 1.0
+    hyperperiod = lcm_args(*[int(item.period) for item in task_set])
+    step = 0.1
+    last_utilization = None
+
+    while True:
+        test_set = copy.deepcopy(task_set)
+
+        for task in test_set:
+            task.exectime *= multiplier
+
+        scheduler = EDFscheduler()
+        completed = scheduler.generate_schedule_with_switches(
+            hyperperiod, test_set, context_switch_time,
+            is_preemptive=is_preemptive)
+
+        schedulable = True
+        for item in completed:
+            if item.finish - item.release > item.deadline:
+                schedulable = False
+
+        print multiplier, "%.4lf" % compute_utilization(test_set)
+
+        if last_utilization is None:
+            last_utilization = compute_utilization(test_set)
+        if schedulable is False:
+            if step < 10**-4:
+                break
+            else:
+                step /= 2
+                multiplier -= step
+        else:
+            last_utilization = compute_utilization(test_set)
+            multiplier += step
+
+    print last_utilization
+    return last_utilization
 # ------------------------------------------------------------------------------
